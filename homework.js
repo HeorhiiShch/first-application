3f5e78c1ed90a745c332c5f13e07e04e3e279fd7
Heorhii <scherbagora@gmail.com>
2023-11-06 16:59:42 +0200

    version 1

---
+++ homework.js
@@ -0,0 +1,12 @@
+const numbersOfMovies = +prompt("How many movies have you wached?", "");
+let personalMovieDB = {
+    count: numbersOfMovies,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const lastWachedMovie = prompt("What was the last movie that you wach?", ""),
+      markForMovie = +prompt("How can you rate it?", "");
+personalMovieDB.movies[lastWachedMovie] = markForMovie;
+console.log(personalMovieDB);
